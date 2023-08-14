9e03d1f24b79a4662bc5f000a1eda9bb4295b6ac
Shan <perera.alc2000@gmail.com>
2023-08-14 12:30:28 +0530

    face-detector implemented (basics)

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print()
+import cv2
+
+# Load the pre-trained face detection classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Open the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    try:
+        # Capture each frame from the webcam
+        ret, frame = cap.read()
+
+        # Convert the frame to grayscale for face detection
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the grayscale frame
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        # Check if faces are detected
+        if len(faces) > 0:
+            print("Face detected")
+
+        # Draw rectangles around the detected faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Display the frame with face detection
+        cv2.imshow('Face Detection', frame)
+
+        # Break the loop when the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    except Exception as e:
+        print("An error occurred:", e)
+        break
+
+# Release the webcam and close the window
+cap.release()
+cv2.destroyAllWindows()
